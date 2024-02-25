8fd886f9dd911d0cb173597f9b41d94e601192a1
yr5913 <yr5913@rit.edu>
2024-02-24 22:20:02 -0500

    add a feature to update profile

--- src/main/java/com/yugeshreganti/school/controller/ProfileController.java
+++ src/main/java/com/yugeshreganti/school/controller/ProfileController.java
@@ -1,18 +1,30 @@
 package com.yugeshreganti.school.controller;
 
+import com.yugeshreganti.school.model.Address;
 import com.yugeshreganti.school.model.Person;
 import com.yugeshreganti.school.model.Profile;
+import com.yugeshreganti.school.repository.PersonRepository;
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
 public class ProfileController {
+
+
+
+    @Autowired
+    PersonRepository personRepository;
 
 
     @RequestMapping("/displayProfile")
@@ -28,5 +40,25 @@ public class ProfileController {
         ModelAndView modelAndView = new ModelAndView("profile.html");
         modelAndView.addObject("profile", profile);
         return modelAndView;
+    }
+
+    @PostMapping(value = "/updateProfile")
+    public String updateProfile(@Valid @ModelAttribute("profile") Profile profile, Errors errors,
+                                HttpSession session)
+    {
+        if(errors.hasErrors()){
+            return "profile.html";
+        }
+        Person person = (Person) session.getAttribute("loggedInPerson");
+        person.setName(profile.getName());
+        person.setEmail(profile.getEmail());
+        person.setMobileNumber(profile.getMobileNumber());
+        if(person.getAddress() ==null || !(person.getAddress().getAddressId()>0)){
+            person.setAddress(new Address());
+        }
+        BeanUtils.copyProperties(profile, person.getAddress());
+        Person savedPerson = personRepository.save(person);
+        session.setAttribute("loggedInPerson", savedPerson);
+        return "redirect:/displayProfile";
     }
 }
