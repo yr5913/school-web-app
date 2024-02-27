041ce556e5f3a6b7744af69bb9f47de1c06cc6f7
yr5913 <yr5913@rit.edu>
2024-02-26 23:23:41 -0500

    Using @DeleteMapping to delete contact message and using RequestEntity object to get request details

--- README.md
+++ README.md
@@ -116,3 +116,4 @@ Follow these steps to set up and run the Spring security Learner Microservice:
 74. Using @RestController for the rest endpoints instead of using spring mvc style
 75. Using @RequestHeader to get header value and also using ResponseEntity to populate response status, header and body in the response
 76. Fixed CSRF issue for Post Requests and fixed authentication methods
+77. Using @DeleteMapping to delete contact message and using RequestEntity object to get request details
--- src/main/java/com/yugeshreganti/school/rest/ContactRestController.java
+++ src/main/java/com/yugeshreganti/school/rest/ContactRestController.java
@@ -5,11 +5,14 @@ import com.yugeshreganti.school.repository.ContactRepository;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -47,6 +50,23 @@ public class ContactRestController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .header("isMsgSaved", "true")
+                .body(response);
+    }
+
+    @DeleteMapping("/deleteMsg")
+    public ResponseEntity<Response> deleteMsg(RequestEntity<Contact> requestEntity) {
+        HttpHeaders headers = requestEntity.getHeaders();
+        headers.forEach((key, value) -> {
+            log.info(String.format(
+                    "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+        });
+        Contact contact = requestEntity.getBody();
+        contactRepository.deleteById(contact.getContactId());
+        Response response = new Response();
+        response.setStatusCode("200");
+        response.setStatusMsg("Message successfully deleted");
+        return ResponseEntity
+                .status(HttpStatus.OK)
                 .body(response);
     }
 
